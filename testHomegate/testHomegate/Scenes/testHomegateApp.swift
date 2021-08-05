5611ae0c47328eda6c84bb6d6fca75fd5a7202da
Pietro Gandolfi <info@sherden.io>
2021-08-05 07:04:57 +0200

    First project setup.

---
+++ testHomegate/testHomegate/Scenes/testHomegateApp.swift
@@ -0,0 +1,17 @@
+//
+//  testHomegateApp.swift
+//  testHomegate
+//
+//  Created by Pietro Gandolfi on 05.08.21.
+//
+
+import SwiftUI
+
+@main
+struct testHomegateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
